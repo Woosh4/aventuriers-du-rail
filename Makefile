0e3be4a154443626d4e22eb86a59662c82a1c730
Woosh4 <alexis.lefevre2004@free.fr>
2025-03-13 09:47:20 +0100

    connect OK, print board OK

--- Makefile
+++ Makefile
@@ -1,15 +1,28 @@
 
-all  : aventuriers
 
-aventuriers : aventurier.c
-	gcc -Wall -o aventurier.c aventuriers
+
+all  : aventurier.o ../TicketToRideAPI/tickettorideapi/codingGameServer.o ../TicketToRideAPI/tickettorideapi/ticketToRide.o
+	gcc -o dab aventurier.o ../TicketToRideAPI/tickettorideapi/codingGameServer.o ../TicketToRideAPI/tickettorideapi/ticketToRide.o
+
+aventurier.o : aventurier.c
+	gcc -c -o aventurier.o aventurier.c
+
+../TicketToRideAPI/tickettorideapi/codingGameServer.o : ../TicketToRideAPI/tickettorideapi/codingGameServer.c ../TicketToRideAPI/tickettorideapi/codingGameServer.h
+	gcc -c -o ../TicketToRideAPI/tickettorideapi/codingGameServer.o ../TicketToRideAPI/tickettorideapi/codingGameServer.c
+
+../TicketToRideAPI/tickettorideapi/ticketToRide.o : ../TicketToRideAPI/tickettorideapi/ticketToRide.c ../TicketToRideAPI/tickettorideapi/ticketToRide.h
+	gcc -c -o ../TicketToRideAPI/tickettorideapi/ticketToRide.o ../TicketToRideAPI/tickettorideapi/ticketToRide.c
 
 clean :
 	rm -f *.o
-	rm -f aventuriers
+	rm -f ../TicketToRideAPI/tickettorideapi/*.o
+	rm -f dab
 
 go : clean all
-	./aventuriers
+	./dab
 
-debug : 
-	gcc -Wall -g -o aventuriers aventurier.c
+debug : clean
+	gcc -c -g -o ticketToRide.o ticketToRide.c
+	gcc -c -g -o codingGameServer.o codingGameServer.c
+	gcc -c -g -o aventurier.o aventurier.c
+	gcc -g -o dab aventurier.o codingGameServer.o ticketToRide.o
--- aventurier.c
+++ aventurier.c
@@ -1,10 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "ticketToRide.h"
+
+/*GameData* gamedata = {
+    //nom partie
+    "partie Alexis";
+    //game seed
+    1234;
+    // starter 1 ou 2
+    2;
+    //nb cities
+    20;
+    //nb tracks
+    40;
 
 
+};*/
 
+void print_gamedata(GameData data){
+    printf("game name : %s\n",data.gameName);
+    printf("game seed : %d\n",data.gameSeed);
+    printf("starter : 0 you, 1 other player : %d\n",data.starter);
+    printf("number of cities : %d\n",data.nbCities);
+    printf("nombre de voies : %d\n",data.nbTracks);
+    printf("number of cards : Nan\n");
+    printf("trackdata : Nan\n");
+    printf("cardscolor : Nan\n");
+}
 
 int main(){
-    printf("ayo");
+    extern int DEBUG_LEVEL;
+    DEBUG_LEVEL = MESSAGE;  
+    int* envoi;
+    MoveData mymove;
+    MoveResult mymoveresult;
+    GameData mygamedata;
+    GameSettings MySettings = GameSettingsDefaults;
+    MySettings.gameType = TRAINING;
+    MySettings.reconnect = 0;
+
+    printf("init OK\n");
+
+    int connect = connectToCGS("cgs.valentin-lelievre.com", 15001);
+    printf("connected? : code %d\n", connect);
+    sendName("Alexisv10");
+    printf("Name sent.\n");
+    sendGameSettings(MySettings, &mygamedata);
+    printf("Game settings sent");
+
+    
+
+    while(1){
+        print_gamedata(mygamedata);
+        printBoard();
+        mymove.action = scanf("move: CLAIM_ROUTE:1, DRAW_BLIND_CARD:2, DRAW_CARD:3, DRAW_OBJECTIVES:4, CHOOSE_OBJECTIVES:5\n");
+
+        sendMove(&mymove,&mymoveresult);
+    }
+
+    sendMessage("fin");
+
     return 0;
 }
